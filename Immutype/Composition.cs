15da2fff5fb0e94a2bfa4336c2d9854cc57f9894
Nikolay Pianikov <nikolayp@mail.ru>
2024-08-29 13:05:03 +0300

    Update deps

--- Immutype/Composition.cs
+++ Immutype/Composition.cs
@@ -10,30 +10,24 @@ using Pure.DI;
 
 internal partial class Composition
 {
-    private static void Setup() => DI.Setup(nameof(Composition))
+    private static void Setup() => DI.Setup()
+        .Root<ISourceBuilder>(nameof(SourceBuilder))
+        .Root<IComponentsBuilder>(nameof(ComponentsBuilder))
+        .Root<ITypeSyntaxFilter>(nameof(SyntaxFilter))
+        
         .DefaultLifetime(Lifetime.PerBlock)
-        
-        .Root<ISourceBuilder>("SourceBuilder")
-        .Root<IComponentsBuilder>("ComponentsBuilder")
-        .Root<ITypeSyntaxFilter>("SyntaxFilter")
-        
-        .Bind()
-            .To<ImmutableArray<TT>>(ctx =>
-            {
-                ctx.Inject(out TT[] arr);
-                return ImmutableArray.Create(arr);
-            })
-        .Bind().To<SourceBuilder>()
-        .Bind().To<SyntaxNodeFactory>()
-        .Bind().To<NameService>()
-        .Bind().To<ExtensionsFactory>()
-        .Bind().To<MethodsFactory>()
-        .Bind().To<DataContainerFactory>()
-        .Bind(Tag.Type).To<MethodWithFactory>()
-        .Bind(Tag.Type).To<MethodAddRemoveFactory>()
-        .Bind().To<CommentsGenerator>()
+            .Bind().To((TT[] arr) => ImmutableArray.Create(arr))
+            .Bind().To<SourceBuilder>()
+            .Bind().To<SyntaxNodeFactory>()
+            .Bind().To<NameService>()
+            .Bind().To<ExtensionsFactory>()
+            .Bind().To<MethodsFactory>()
+            .Bind().To<DataContainerFactory>()
+            .Bind(Tag.Type).To<MethodWithFactory>()
+            .Bind(Tag.Type).To<MethodAddRemoveFactory>()
+            .Bind().To<CommentsGenerator>()
     
         .DefaultLifetime(Lifetime.Singleton)
-        .Bind().To<ComponentsBuilder>()
-        .Bind().To<TypeSyntaxFilter>();
+            .Bind().To<ComponentsBuilder>()
+            .Bind().To<TypeSyntaxFilter>();
 }
